f67a36bdc87e33100533a68f429741cd50148af5
Krystle Taniguchi <ktanig87@gmail.com>
2021-11-18 09:13:45 -0700

    Finish Controllers

--- MoveManaged.WebMVC/Controllers/RoomController.cs
+++ MoveManaged.WebMVC/Controllers/RoomController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using MoveManaged.Models;
+using MoveManaged.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,91 @@ namespace MoveManaged.WebMVC.Controllers
         // GET: Room
         public ActionResult Index()
         {
-            return View();
+            var service = CreateRoomService();
+            var model = service.GetRooms();
+            return View(model);
+        }
+        
+        public ActionResult Create()
+        { return View(); }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(RoomCreate model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var service = CreateRoomService();
+
+            if(service.CreateMove(model))
+            {
+                TempData["SaveResult"] = "Your room was created";
+                return RedirectToAction("index");
+            };
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateRoomService();
+            var detail = service.GetRoombyId(id);
+            var model =
+                new RoomEdit
+                {
+                    RoomId = detail.RoomId,
+                    RoomName = detail.RoomName,
+                    MoveId = detail.MoveId
+                };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, RoomEdit model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            if (model.RoomId !=id)
+            {
+                ModelState.AddModelError("", "Id does not match");
+                return View(model);
+            }
+            var service = CreateRoomService();
+
+            if(service.UpdateRoom(model))
+            {
+                TempData["SaveResult"] = "Your Room has been updated";
+                return RedirectToAction("index");
+            }
+            ModelState.AddModelError("", "Your Room could not be updated");
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateRoomService();
+            var model = svc.GetRoombyId(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoom(int id)
+        {
+            var service = CreateRoomService();
+            service.DeleteRoom(id);
+            TempData["SaveResult"] = "You Room was successfully deleted";
+            return RedirectToAction("index");
+        }
+
+
+        private RoomService CreateRoomService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new RoomService(userId);
+            return service;
         }
     }
 }
