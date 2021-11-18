62d57c6315dfec354aac0ce8deaa135939b23d22
Krystle Taniguchi <ktanig87@gmail.com>
2021-11-18 09:21:58 -0700

    identity config adjustments

--- MoveManaged.WebMVC/App_Start/IdentityConfig.cs
+++ MoveManaged.WebMVC/App_Start/IdentityConfig.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using MoveManaged.WebMVC.Data;
 using MoveManaged.WebMVC.Models;
 
 namespace MoveManaged.WebMVC
--- MoveManaged.WebMVC/Controllers/AccountController.cs
+++ MoveManaged.WebMVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using MoveManaged.WebMVC.Data;
 using MoveManaged.WebMVC.Models;
 
 namespace MoveManaged.WebMVC.Controllers
